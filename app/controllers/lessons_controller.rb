3f2dc81e8b9904d8d89f181c15e2833d54919fa0
Sarah Humphreys <sarah.humphreys50@gmail.com>
2019-04-09 01:04:28 +0000

    congigured authorization for enrollments

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,18 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :enrolled_in_lesson
+
 	def show
+
 	end
-	
+
 	private
+
+	def enrolled_in_lesson
+		unless current_user&.enrolled_in?(current_lesson.section.course) 
+			redirect_to course_path, alert: 'You must enroll'
+		end	
+	end		
 
 	helper_method :current_lesson
 	def current_lesson
