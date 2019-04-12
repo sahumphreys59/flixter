606ae6a364e30d1ffc9279658f0f9da85aed5901
Sarah Humphreys <sarah.humphreys50@gmail.com>
2019-04-12 00:19:29 +0000

    stuck with section position

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,8 @@ Rails.application.routes.draw do
 	end	
 	resources :lessons, only: [:show]
 	namespace :instructor do
-		resources :sections, only: [:update]
 		resources :lessons, only: [:update]
-		resources :sections, only: []do
+		resources :sections, only: [:update] do
 			resources :lessons, only: [:new, :create]
 		end	
 		resources :courses, only: [:new, :create, :show] do
